9cbd27719a151d7268e5937abb077f1b47368dcd
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-10-15 17:09:46 +0700

    upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState, type FormEvent } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { GoogleGenAI } from '@google/genai'
+
+function App() {
+  return (
+    <>
+      WORKSHOP “XÂY CHATBOT AI VỚI GEMINI TRONG 2H”
+    </>
+  )
+}
+
+export default App
